0642c4b62d0e46a097fb990b65ccc0241945418e
donvine mugendi <donvinemugendi@gmail.com>
2023-05-21 01:27:17 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const[isPurchased, setIsPurchased]=useState(false);
+  const Class= !isPurchased?"":"in-cart";
+  function handlePurchase(){
+    setIsPurchased(!isPurchased);
+  }
   return (
-    <li className="">
+    <li className={Class}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handlePurchase}>{!isPurchased? "Add to Cart":"Remove from Cart"}</button>
     </li>
   );
 }
