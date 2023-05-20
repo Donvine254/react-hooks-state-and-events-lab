0225551fdc9f1f6e8a264f33420afe5124093f46
donvine mugendi <donvinemugendi@gmail.com>
2023-05-21 01:44:36 +0300

    updated theme button

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,8 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
   const [isDark, setIsDark] = useState(false);
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+  const themeBtn=(isDark)?"Dark Mode":"Light Mode"
   const appClass = isDark ? "App dark" : "App light"
   function toggleTheme(){
    setIsDark(!isDark)
@@ -16,7 +14,7 @@ function App() {
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={toggleTheme}>{(isDark)?"Dark Mode":"Light Mode"}</button>
+        <button onClick={toggleTheme}>{themeBtn}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
