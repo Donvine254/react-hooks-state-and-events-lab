9c4a07dc7c44dac29eb2846f3115f2ba46b6bb27
donvine mugendi <donvinemugendi@gmail.com>
2023-05-21 01:41:19 +0300

    updated tenary expressions for simplicity

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,20 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
-  const[isPurchased, setIsPurchased]=useState(false);
-  const Class= !isPurchased?"":"in-cart";
-  const BtnClass= !isPurchased?"add":"remove";
-  const BtnText=!isPurchased? "Add to Cart":"Remove from Cart"
-  function handlePurchase(){
+  const [isPurchased, setIsPurchased] = useState(false);
+  const Class = isPurchased ? "in-cart" : "";
+  const BtnClass = isPurchased ? "remove" : "add";
+  const BtnText = isPurchased ? "Remove from Cart" : "Add to Cart";
+  function handlePurchase() {
     setIsPurchased(!isPurchased);
   }
   return (
     <li className={Class}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className={BtnClass} onClick={handlePurchase}>{BtnText}</button>
+      <button className={BtnClass} onClick={handlePurchase}>
+        {BtnText}
+      </button>
     </li>
   );
 }
